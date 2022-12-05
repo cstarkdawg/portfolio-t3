b412449c6201ebcbb9ef803588ed9ea69c937ea6
cstarkdawg <cstarkey5@gmail.com>
2022-12-05 23:41:06 +0800

    Update little-nangang-mountain.tsx

--- src/pages/little-nangang-mountain.tsx
+++ src/pages/little-nangang-mountain.tsx
@@ -32,38 +32,33 @@ const Home: NextPage = () => {
         />
       </a>
       <main className="bg-white text-slate-700">
-      <img src="./assets/projects/little-nangang-mountain.jpg" alt="Little Nangang Mountain"/>
       <br></br>
     <h1 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-4xl">Little Nangang Mountain (小南港山)</h1>
-    <body>
-      <br></br>
+      <p>
+      <img src="./assets/projects/little-nangang-mountain.jpg" alt="Little Nangang Mountain"/>
+      </p>
       <p>
       This one is fantastic if you are looking for something simple and easily accessible by transit.
       </p>
-<br></br>
-<p>Just a 7-minute walk from Nangang Exhibition Center Station will get you to the start of the Little Nangang Mountain trail. Hike 10-15 minutes up a series of stairs through a small forest, and you’ll reach the top of the mountain.
+<p>
+  Just a 7-minute walk from Nangang Exhibition Center Station will get you to the start of the Little Nangang Mountain trail. Hike 10-15 minutes up a series of stairs through a small forest, and you’ll reach the top of the mountain.
 </p>
-<br></br>
 <p>
 On the way up, you’ll pass by a temple (終南洞三聖宮). To the side and behind the temple are a couple of places to view surrounding scenery as well as catch the sunset, although the view is obstructed by trees. Stay here if you like, or keep walking a few minutes to the top of the main staircase.
 </p>
-<br></br>
 <p>
 Once you arrive at the peak, there is one main sunset-viewing platform, and really only one good angle on top of the mountain to see the sunset. So if there are other people, you might have to compete for picture-taking space. When I’ve gone during the week, there’s only been one or two other people up there around sunset time.
 </p>
-<br></br>
 <p>
 The view of Nangang and Xinyi in the distance is pretty good, although the view from the main platform is obstructed in the middle by a tree, making it impossible to see the full skyline unless you have a drone.
 </p>
-<br></br>
 <p>
 You can also go to a different viewpoint adjacent from the main platform that gives a great view of Xizhi ahead and Neihu/Songshan to the west. Depending on the season, you might also be able to catch a glimpse of the sun from here as it nears the horizon.
 </p>
-<br></br>
 <p>
 This could be considered a more laid-back alternative to its bigger brother in the distance, Nangang Mountain, which is connected to the Four Beasts Mountain series of trails (these include the classic Elephant Mountain). Little Nangang Mountain is much easier to get to by MRT than Nangang Mountain, which requires a long walk from near Houshanpi Station on the Blue Line or Xiangshan Station on the Red Line.
 </p>
-</body>
+
 
       </main>
     </>
